9f53d083d291f355d1e15048f421c4fb60ed5d8f
Lavender <susie204@naver.com>
2023-08-07 12:03:32 +0900

    Initial Commit

---
+++ SeSACWeek4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACWeek4
+//
+//  Created by 이승현 on 2023/08/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
